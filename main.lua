cb4e68adef7e6e6e48f9d90e899e5c67154535c6
atsoares <aleexsoarees@gmail.com>
2016-04-08 00:11:02 -0300

    Update main.lua
    jogabilidade terminada (falta travar o jogo* apos monstro alcancar heroi, ele para de spawnar monstros mas continua rodando)
    adicionado score ok
    mudanca de sprite durante troca de arma ok (falta colocar os novos sprites)

--- main.lua
+++ main.lua
@@ -10,6 +10,11 @@ physics.start()
     local vilao = {}
     local i = 1
     local vilaoTimer
+	local score
+	
+	local function updateTexto()
+    textScore.text = "Score: "..score
+end
 ------- Background beginning --------------------
 
     --Hide status bar from the beginning
@@ -105,7 +110,7 @@ physics.start()
 		local sheet_heroi = graphics.newImageSheet( "images/mrball.png", mrbola )
 		
 		-- tabela de sequencia dos frames
-		local pulando_heroi = {
+		local sequencia_heroi = {
 			-- sequencia dos frames
 			{
 				name = "pulando",
@@ -114,16 +119,23 @@ physics.start()
 				time = 700,
 				loopCount = 0,
 				loopDirection = "forward"
+			},
+			{
+				name = "parado",
+				frames = {16},
+				time = 700,
+				loopCount = 0,
+				loopDirection = "forward"
 			}
 		}
 		
-		local heroi = display.newSprite( sheet_heroi, pulando_heroi )
+		local heroi = display.newSprite( sheet_heroi, sequencia_heroi )
 		heroi.x = 160
-		heroi.y = 370
+		heroi.y = 400
 		arma.quadrado = 1
 		arma.triangulo = 2
 		heroi.arma = 1
-		--physics.addBody(heroi, "kinematic", {bounce = 0})
+		---physics.addBody(heroi, "kinematic", {bounce = 0})
 		heroi:setSequence("pulando")
 		heroi:play()
 		heroi.collision = onCollision
@@ -134,61 +146,81 @@ physics.start()
 		if (event.numTaps == 2) then	
 			if (heroi.arma == 1) then
 			heroi.arma = 2
-			print( "Arma mudou para triangulo" )
+			heroi:setSequence("parado")
+			print( "Arma vermelha" )
 			return true;
 			end
 			elseif (heroi.arma == 2) then
+			heroi:setSequence("pulando")
+			heroi:play()
 			heroi.arma = 1
-			print( "Arma mudou para quadrado" )
+			print( "Arma verde" )
 			return true;
 			end
 		end
 
 		heroi:addEventListener( "tap" )
 		
-local levelGroup = display.newGroup()
+-- local levelGroup = display.newGroup()
 
-		local function onCollision(self, event)
+		-- local function onCollision(self, event)
 		
-		if self.name == "heroi" and event.other.name == "vilao" then
+		-- if self.name == "heroi" and event.other.name == "vilao" then
 
-	        local gameoverText = display.newText("Game Over!", 0, 0, "HelveticaNeue", 35)
-	        gameoverText:setTextColor(255, 255, 255)
-	        gameoverText.x = display.contentCenterX
-	        gameoverText.y = display.contentCenterY
-	        gameLayer:insert(gameoverText)
+	        -- local gameoverText = display.newText("Game Over!", 0, 0, "HelveticaNeue", 35)
+	        -- gameoverText:setTextColor(255, 255, 255)
+	        -- gameoverText.x = display.contentCenterX
+	        -- gameoverText.y = display.contentCenterY
+	        -- gameLayer:insert(gameoverText)
 
 	        -- This will stop the gameLoop
-	        gameIsActive = false
-	    end
+	        -- gameIsActive = false
+	    -- end
 	
-		end
+		-- end
 
 ------- Spawning quadrados ----------------------------
 
 function onTouch(self, event)
 	if (heroi.arma == self.id) then
+	score=score+10
+	updateTexto()
     display.remove(self)
 	return true;
-	end
+	end	
 end
 
-		-- local andando = function(event)
-		-- vilao[i].y = vilao[i].y +0.4
-		-- return true
-		-- end		
 		
+local function andando(self,event)
+	if (self.y ~= nil) then
+		self.y = self.y + 2
+		if (self.y == heroi.y) then
+		gameIsActive = false
+		print (gameIsActive)
+		return gameIsActive
+		
+		end
+		
+	end
+end
+		
+		
+score = 0
+textScore = display.newText("Score: "..score, 5, 2, "VarelaRound-Regular", 15)
+    textScore:setTextColor(0,0,255)
 local spawnQuad = function()
 
-if gameIsActive then
+if (gameIsActive == true) then
+
 ------ Sprite quadradovilao pulando --------------------
 		local quadradovilao= {
 		   width = 82,
 		   height = 82,
 		   numFrames = 18
 		}
-		local viloes = math.random(1,2)
-		local sheet_quad = graphics.newImageSheet( "images/squarehead_"..viloes..".png", quadradovilao )
+		-- local viloes = math.random(1,2)
+		local sheet_quad = graphics.newImageSheet( "images/squarehead_1.png", quadradovilao )
+		local sheet_quad2 = graphics.newImageSheet( "images/squarehead_2.png", quadradovilao )
 		
 		-- tabela de sequencia dos frames
 		local quad_pulando = {
@@ -203,28 +235,47 @@ if gameIsActive then
 			}
 		}
 ------- Sprite quadradovilao pulando fim--------------------
-
-
-
+		
+		
     	local Fall = math.random(2) == 1 and 220 or 100
-		vilao[i] = display.newSprite( sheet_quad, quad_pulando )
-    	vilao[i].x = Fall
-    	vilao[i].y = -30   	   	
-    	vilao[i].value = i 
-		vilao[i].id = 1
-		physics.addBody(vilao[i], "dynamic", {bounce = 0}) 
-		vilao[i]:setSequence("pulando")
-		vilao[i]:play()	
-		vilao[i].touch = onTouch
-		vilao[i]:addEventListener("touch")
-		
-    	i = i + 1		
+		local monstro = math.random(1, 2)
+		if (monstro == 1) then
+			vilao[i] = display.newSprite(sheet_quad, quad_pulando )
+			vilao[i].id = 1
+			elseif (monstro == 2) then
+			vilao[i] = display.newSprite(sheet_quad2, quad_pulando )
+			vilao[i].id = 2
+		end
+			vilao[i].x = Fall
+			vilao[i].y = -30   	   	
+			vilao[i].value = i 
+
+			
+			--physics.addBody(vilao[i], "dynamic", {bounce = 0}) 
+			vilao[i]:setSequence("pulando")
+			vilao[i]:play()	
+			vilao[i].touch = onTouch
+			vilao[i]:addEventListener("touch")
+			vilao[i].enterFrame = andando
+			
+			Runtime:addEventListener("enterFrame", vilao[i])
+
+			
+    	
+	
 		
 	
+    	i = i + 1
+	
+		
 end
+
 	
+			
     end
-    vilaoTimer = timer.performWithDelay( 1500, spawnQuad, -1 ) -- this is a time and 1000 is 1 second and 1000 miliseconds.. it will spawn one circle every one second.. you can change this at your own liking..
+	
+	
+    vilaoTimer = timer.performWithDelay( 800, spawnQuad, -1 ) -- this is a time and 1000 is 1 second and 1000 miliseconds.. it will spawn one circle every one second.. you can change this at your own liking..
 
 ------- Spawning quadrados fim -------------------------
 
