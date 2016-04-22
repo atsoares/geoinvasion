12950c25272789d177b662e820ac8b9ed9744822
Alexander Soares <aleexsoarees@gmail.com>
2016-04-22 14:11:09 -0300

    Update main.lua
    Novos sprites e controle de sprites por ação (triggers), 
    Adição do powerUp funcionando corretamente.

--- main.lua
+++ main.lua
@@ -54,9 +54,11 @@
 	local gameIsActive = true
 	local vilao = {}
     local i = 1
-    local vilaoTimer, score, distancia, verificador, verifica
+    local vilaoTimer, score, distancia, verifica, verificaDistancia, verificaPowerUp, monstrosEliminados, 
+	contadorPowerUP, dificuldade, powerUp
+	local powerUpAtivado = false
 	local metro = false
-	local arma = {'quadrado', 'triangulo'}
+	local arma = {'quadrado', 'triangulo', 'especial'}
 	local sentido = {'esquerda', 'direita'}
 	local physics = require("physics")
     physics.setScale( 60 )
@@ -65,21 +67,36 @@
 	physics.start()
 	score = 0
 	distancia = 0
+	dificuldade = 800
 	verifica = 10
+	monstrosEliminados = 0
+	contadorPowerUP = 0
+	powerUp = false
 	
 	local function updateTexto()
     textScore.text = "Score: "..score
-    textDistancia.text = "Distância: "..distancia
+    textDistancia.text = "Distância: "..distancia.." m"
 	end
 	
 	local function verificarMetro()
-	verificador = distancia/verifica
-	if (verificador == 1) then 
+	verificaDistancia= distancia/verifica
+	if (verificaDistancia == 1) then 
 	verifica = verifica + 10
 	metro = true
 	return true
 	end 
 	end	
+
+	local function rubikCube()
+	powerUp = true
+	end
+	
+	local somAtirando = audio.loadSound("audio/atirando.wav")
+	local somAtirandoPowerUp = audio.loadSound("audio/atirandoPowerUp.wav")
+	local coletandoPowerUp = audio.loadSound("audio/coletarPowerUp.wav")
+	local somTrocandoArma = audio.loadSound("audio/trocarArma.wav")
+	local coletandoEstrela = audio.loadSound("audio/coletarEstrela.wav")
+	
 ------- Background beginning --------------------
 
     --Hide status bar from the beginning
@@ -91,17 +108,17 @@
     scrollSpeed = 0.7; -- Set Scroll Speed of background
      
     -- Add First Background
-    local bg1 = display.newImageRect("images/rua.png", 320, 480)
+    local bg1 = display.newImageRect("images/moonground.png", 320, 480)
     bg1:setReferencePoint(display.CenterLeftReferencePoint)
     bg1.x = 0; bg1.y = _H/2;
      
     -- Add Second Background
-    local bg2 = display.newImageRect("images/rua.png", 320, 480)
+    local bg2 = display.newImageRect("images/moonground.png", 320, 480)
     bg2:setReferencePoint(display.CenterLeftReferencePoint)
     bg2.x = 0; bg2.y = bg1.y+480;
      
     -- Add Third Background
-    local bg3 = display.newImageRect("images/rua.png", 320, 480)
+    local bg3 = display.newImageRect("images/moonground.png", 320, 480)
     bg3:setReferencePoint(display.CenterLeftReferencePoint)
     bg3.x = 0; bg3.y = bg2.y+480;
      
@@ -129,12 +146,29 @@
     Runtime:addEventListener( "enterFrame", move )
 	
 ------- Background end ---------------------
-	
+		local rubik = {
+			width = 30,
+			height = 30,
+			numFrames = 11
+		}
+		local sheet_rubik = graphics.newImageSheet ("images/rubikscube.png", rubik)
+		
+		local animacao_rubik = {
+		{
+			name = "rubik_girando",
+			start = 1,
+			count = 11,
+			time = 800,
+			loopCount = 0,
+			loopDirection = "bounce"
+			}
+		}
+		
 ------- Sprite bolaheroi pulando --------------------
 		local astronauta = {
 		   width = 80,
 		   height = 70,
-		   numFrames = 38
+		   numFrames = 58
 		}
 		local sheet_heroi = graphics.newImageSheet( "images/astronauta.png", astronauta )
 		
@@ -171,7 +205,7 @@
 			},
 			{
 				name = "laranjaDir_andando",
-				frames = {7,8},
+				frames = {7,8,7,8},
 				time = 700,
 				loopCount = 0,
 				loopDirection = "forward"
@@ -192,7 +226,7 @@
 			},
 			{
 				name = "verdeEsq_andando",
-				frames = {12,13},
+				frames = {12,13,12,13},
 				time = 700,
 				loopCount = 0,
 				loopDirection = "forward"
@@ -213,7 +247,7 @@
 			},
 			{
 				name = "verdeDir_andando",
-				frames = {17,18},
+				frames = {17,18,17,18},
 				time = 700,
 				loopCount = 0,
 				loopDirection = "forward"
@@ -225,26 +259,144 @@
 				loopCount = 1,
 				loopDirection = "forward"
 			},	
+			{
+				name = "rubikEsq_parado",
+				frames = {21},
+				time = 700,
+				loopCount = 0,
+				loopDirection = "forward"
+			},	
+			{
+				name = "rubikEsq_andando",
+				frames = {22,23,24,25},
+				time = 700,
+				loopCount = 0,
+				loopDirection = "forward"
+			},	
+			{
+				name = "rubikEsq_atirando",
+				frames = {26,27,28,29},
+				time = 700,
+				loopCount = 1,
+				loopDirection = "forward"
+			},	
+			{
+				name = "rubikDir_parado",
+				frames = {30},
+				time = 700,
+				loopCount = 0,
+				loopDirection = "forward"
+			},	
+			{
+				name = "rubikDir_andando",
+				frames = {31,32,33,34},
+				time = 700,
+				loopCount = 0,
+				loopDirection = "forward"
+			},	
+			{
+				name = "rubikDir_atirando",
+				frames = {35,36,37,38},
+				time = 700,
+				loopCount = 1,
+				loopDirection = "forward"
+			},	
+			{
+				name = "trocandoParaArmaLaranja_esq",
+				frames = {46,47,48,49,50,51,52},
+				time = 400,
+				loopCount = 1,
+				loopDirection = "forward"
+			},	
+			{
+				name = "trocandoParaArmaLaranja_dir",
+				frames = {39,40,41,42,43,44,45},
+				time = 400,
+				loopCount = 1,
+				loopDirection = "forward"
+			},	
+			{
+				name = "trocandoParaArmaVerde_esq",
+				frames = {52,51,50,49,48,47,46},
+				time = 400,
+				loopCount = 1,
+				loopDirection = "forward"
+			},	
+			{
+				name = "trocandoParaArmaVerde_dir",
+				frames = {45,44,43,42,41,40,39},
+				time = 400,
+				loopCount = 1,
+				loopDirection = "forward"
+			},
+			{
+				name = "verde_trocandoParaArmaEspecial_esq",
+				frames = {46,47,48,49,53,54,55},
+				time = 400,
+				loopCount = 1,
+				loopDirection = "forward"
+			},	
+			{
+				name = "verde_trocandoParaArmaEspecial_dir",
+				frames = {39,40,41,42,56,57,58},
+				time = 400,
+				loopCount = 1,
+				loopDirection = "forward"
+			},	
+			{
+				name = "laranja_trocandoParaArmaEspecial_esq",
+				frames = {52,51,50,49,53,54,55},
+				time = 400,
+				loopCount = 1,
+				loopDirection = "forward"
+			},	
+			{
+				name = "laranja_trocandoParaArmaEspecial_dir",
+				frames = {45,44,43,42,56,57,58},
+				time = 400,
+				loopCount = 1,
+				loopDirection = "forward"
+			},	
+			{
+				name = "ArmaEspecial_esq_acabou",
+				frames = {55,54,53,49,48,47,46},
+				time = 400,
+				loopCount = 1,
+				loopDirection = "forward"
+			},	
+			{
+				name = "ArmaEspecial_dir_acabou",
+				frames = {58,57,56,42,41,40,39},
+				time = 400,
+				loopCount = 1,
+				loopDirection = "forward"
+			},
+			
 			
 		}
-
+			
+	
+		
+		
 		
 		local heroi = display.newSprite( sheet_heroi, sequencia_heroi )
 		heroi.x = 160
 		heroi.y = 400
 		arma.quadrado = 1
 		arma.triangulo = 2
+		arma.especial = 3
 		sentido.esquerda = 1
 		sentido.direita = 2
 		heroi.arma = 1
 		heroi.sentido = 2
+		heroi.powerUpAtivado = false
 		
 		---physics.addBody(heroi, "kinematic", {bounce = 0})
 		heroi:setSequence("verdeDir_andando")
 		heroi:play()
 		heroi.collision = onCollision
 		heroi:addEventListener("collision", heroi)
-------- Sprite bolaheroi pulando fim--------------------		
+		
 		function HeroiAtirar (event) --- função que chama os sprites na hora certa
 			local spriteAtual = event.target
 			if (heroi.arma == 1 and heroi.sentido == 1) then
@@ -263,83 +415,22 @@
 				spriteAtual:play()
 				end		
 			elseif (heroi.arma == 2 and heroi.sentido == 2) then
-
 				if (event.phase == "ended") then 
 				spriteAtual:setSequence("laranjaDir_andando")
 				spriteAtual:play()
-				end	
+				end
+			elseif (heroi.arma == 3 and heroi.sentido == 1) then
+				if (event.phase == "ended") then 
+				spriteAtual:setSequence("rubikEsq_andando")
+				spriteAtual:play()
+				end
+			elseif (heroi.arma == 3 and heroi.sentido == 2) then
+				if (event.phase == "ended") then 
+				spriteAtual:setSequence("rubikDir_andando")
+				spriteAtual:play()
+				end
 			end
 		end
-		
-		function heroi:tap(event) --- função para trocar a arma do astronauta
-		if (event.numTaps == 1) then	
-			if (heroi.arma == 1 and heroi.sentido == 2) then
-			heroi.arma = 2
-			heroi:setSequence("laranjaDir_andando")
-			heroi:play()
-			print( "Arma vermelha" )
-			return true;
-			
-			elseif (heroi.arma == 1 and heroi.sentido == 1) then
-			heroi.arma = 2
-			heroi:setSequence("laranjaEsq_andando")
-			heroi:play()
-			print( "Arma vermelha" )
-			return true;
-			
-			elseif (heroi.arma == 2 and heroi.sentido == 2) then
-			heroi.arma = 1
-			heroi:setSequence("verdeDir_andando")
-			heroi:play()
-			print( "Arma vermelha" )
-			return true;
-			
-			elseif (heroi.arma == 2  and heroi.sentido == 1) then
-			heroi.arma = 1
-			heroi:setSequence("verdeEsq_andando")
-			heroi:play()
-			print( "Arma vermelha" )
-			return true;
-			end
-		end
-		end
-
-		heroi:addEventListener( "tap" )
-		
-
-------- Spawning quadrados ----------------------------
-
-	function onTouch(self, event) ------ função destruir monstro 
-		if (heroi.arma == self.id) then
-		if (self.id == 1 and self.x == 100) then
-		heroi:setSequence("verdeEsq_atirando")
-		heroi:play()
-		heroi:addEventListener("sprite", HeroiAtirar)
-		heroi.sentido = 1
-		elseif (self.id == 1 and self.x == 220) then
-		heroi:setSequence("verdeDir_atirando")
-		heroi:play()
-		heroi:addEventListener("sprite", HeroiAtirar)
-		heroi.sentido = 2
-		elseif (self.id == 2 and self.x == 100) then
-		heroi:setSequence("laranjaEsq_atirando")
-		heroi:play()
-		heroi:addEventListener("sprite", HeroiAtirar)
-		heroi.sentido = 1
-		elseif (self.id == 2 and self.x == 220) then
-		heroi:setSequence("laranjaDir_atirando")
-		heroi:play()
-		heroi:addEventListener("sprite", HeroiAtirar)
-		heroi.sentido = 2
-		end
-		display.remove(self)
-		score=score+10
-		updateTexto()
-		
-		return true;
-		end	
-	end
-
 		
 	local function andando(self,event)
 		if (self.y ~= nil) then
@@ -352,12 +443,276 @@
 			end
 			
 		end
+	end	
+		
+	local function powerUpAtivo(event)
+		if (heroi.sentido == 1) then
+		heroi:setSequence("ArmaEspecial_esq_acabou")
+		heroi:play()
+		heroi.arma = 1
+		
+		
+		elseif (heroi.sentido == 2) then
+		heroi:setSequence("ArmaEspecial_dir_acabou")
+		heroi:play()
+		heroi.arma = 1
+		end
+		
+		powerUp = false
+		heroi:addEventListener("sprite", HeroiAtirar)
+		heroi.powerUpAtivado = false
+	end	
+		
+	function OnTouch2(self, event)
+		
+			audio.play(coletandoPowerUp)
+		if (self.x == 100 and heroi.arma == 1) then 
+			heroi:setSequence("verde_trocandoParaArmaEspecial_esq")
+			heroi:play()
+			heroi:addEventListener("sprite", HeroiAtirar)
+			heroi.sentido = 1
+			heroi.arma = 3
+			elseif (self.x == 220 and heroi.arma == 1) then
+			heroi:setSequence("verde_trocandoParaArmaEspecial_dir")
+			heroi:play()
+			heroi:addEventListener("sprite", HeroiAtirar)
+			heroi.sentido = 2
+			heroi.arma = 3 
+			elseif (self.x == 100 and heroi.arma == 2) then
+			heroi:setSequence("laranja_trocandoParaArmaEspecial_esq")
+			heroi:play()
+			heroi:addEventListener("sprite", HeroiAtirar)
+			heroi.sentido = 1
+			heroi.arma = 3 
+			elseif (self.x == 220 and heroi.arma == 2) then
+			heroi:setSequence("laranja_trocandoParaArmaEspecial_dir")
+			heroi:play()
+			heroi:addEventListener("sprite", HeroiAtirar)
+			heroi.sentido = 2
+			heroi.arma = 3 
+			
+		end
+		heroi.powerUpAtivado = true
+		timer.performWithDelay(5000, powerUpAtivo)
+		if(event.phase == "ended") then
+		timer.performWithDelay(1, function() self:removeSelf() end )
+		audio.play(coletandoPowerUp)
+		contadorPowerUP = contadorPowerUP +1
+		dificuldade = dificuldade + 50
+		score=score+100
+		updateTexto()
+		end
+
+		return true
 	end
-				
-	textScore = display.newText("Score: "..score, 6, 2, "VarelaRound-Regular", 15, left)
-    textScore:setTextColor(0,0,255)
-	textDistancia = display.newText("Distância: "..distancia.." m", 5, 15, "VarelaRound-Regular", 15)
+	
+	
+
+	
+			
+	function criarRubik(self)	
+		
+		local rubikPowerUP = display.newSprite( sheet_rubik, animacao_rubik )
+			rubikPowerUP.x = self.x
+			rubikPowerUP.y = self.y
+			rubikPowerUP.id = 3	
+			rubikPowerUP:setSequence("rubik_girando")
+			rubikPowerUP:play()
+			rubikPowerUP.touch = OnTouch2
+			physics.addBody(rubikPowerUP, "dynamic", {bounce = 0}) 
+		
+		rubikPowerUP:addEventListener("touch", rubikPowerUP)
+			rubikPowerUP:toFront()
+			
+	end
+
+-- function rubikPowerUP:touch (event)
+	-- if (self.x == 100) then 
+			-- heroi:setSequence("rubikEsq_andando")
+			-- heroi:play()
+			-- powerUpAtivo()
+			-- heroi.powerUpAtivado = true 
+			-- heroi.sentido = 1
+			
+			
+			-- elseif (self.x == 220) then
+			-- heroi:setSequence("rubikDir_andando")
+			-- powerUpAtivo()
+			-- heroi.powerUpAtivado = true
+			-- heroi:play()
+			-- heroi.sentido = 2
+			
+		-- end
+		-- if(event.phase == "ended") then
+		-- timer.performWithDelay(1, function() self:removeSelf() end )
+				-- score=score+100
+		-- updateTexto()
+		-- end
+	
+	-- return true
+	-- end
+
+	
+------- Sprite bolaheroi pulando fim--------------------		
+
+		
+		-- function heroiTrocarArma(event) ---- função que chama sprite certo após trocar de arma
+		-- local spriteAtual = event.target
+			-- if (heroi.arma == 1 and heroi.sentido == 1) then
+				-- if (event.phase == "ended") then 
+				-- spriteAtual:setSequence("verdeEsq_andando")
+				-- spriteAtual:play()
+				-- end
+			-- elseif (heroi.arma == 1 and heroi.sentido == 2) then
+				-- if (event.phase == "ended") then 
+				-- spriteAtual:setSequence("verdeDir_andando")
+				-- spriteAtual:play()
+				-- end
+			-- elseif (heroi.arma == 2 and heroi.sentido == 1) then
+				-- if (event.phase == "ended") then 
+				-- spriteAtual:setSequence("laranjaEsq_andando")
+				-- spriteAtual:play()
+				-- end		
+			-- elseif (heroi.arma == 2 and heroi.sentido == 2) then
+				-- if (event.phase == "ended") then 
+				-- spriteAtual:setSequence("laranjaDir_andando")
+				-- spriteAtual:play()
+				-- end
+			-- elseif (heroi.arma == 3 and heroi.sentido == 1) then
+				-- if (event.phase == "ended") then 
+				-- spriteAtual:setSequence("rubikEsq_andando")
+				-- spriteAtual:play()
+				-- end
+			-- elseif (heroi.arma == 2 and heroi.sentido == 2) then
+				-- if (event.phase == "ended") then 
+				-- spriteAtual:setSequence("rubikDir_andando")
+				-- spriteAtual:play()
+				-- end
+			-- end
+		-- end
+		
+		function heroi:tap(event) --- função para trocar a arma do astronauta
+		if (event.numTaps == 1 and heroi.arma ~= 3) then
+			audio.play(somTrocandoArma)		
+			if (heroi.arma == 1 and heroi.sentido == 2) then
+			heroi:setSequence("trocandoParaArmaLaranja_dir")
+			heroi:play()
+			heroi:addEventListener("sprite", HeroiAtirar)
+			heroi.arma = 2
+
+			print( "Arma vermelha" )
+			return true;
+			
+			elseif (heroi.arma == 1 and heroi.sentido == 1) then
+			heroi:setSequence("trocandoParaArmaLaranja_esq")
+			heroi:play()
+			heroi:addEventListener("sprite", HeroiAtirar)
+			heroi.arma = 2
+			
+			print( "Arma vermelha" )
+			return true;
+			
+			elseif (heroi.arma == 2 and heroi.sentido == 2) then
+			heroi:setSequence("trocandoParaArmaVerde_dir")
+			heroi:play()
+			heroi:addEventListener("sprite", HeroiAtirar)
+			heroi.arma = 1
+			
+			print( "Arma verde" )
+			return true;
+			
+			elseif (heroi.arma == 2  and heroi.sentido == 1) then
+			heroi:setSequence("trocandoParaArmaVerde_esq")
+			heroi:play()
+			heroi:addEventListener("sprite", HeroiAtirar)
+			heroi.arma = 1
+			print( "Arma verde" )
+			return true;
+			end
+		end
+		end
+
+		heroi:addEventListener( "tap" )
+		
+
+------- Spawning quadrados ----------------------------
+		
+
+
+	function onTouch(self, event) ------ função destruir monstro 
+		
+		if (heroi.arma == 3 and heroi.powerUpAtivado == true) then
+			if (self.x == 100) then
+				heroi:setSequence("rubikEsq_atirando")
+				heroi:play()
+				heroi:addEventListener("sprite", HeroiAtirar)
+				heroi.sentido = 1
+				elseif (self.x == 220) then
+				heroi:setSequence("rubikDir_atirando")
+				heroi:play()
+				heroi:addEventListener("sprite", HeroiAtirar)
+				heroi.sentido = 2
+			end
+			audio.play(somAtirandoPowerUp)
+			display.remove(self)
+		score=score+10
+		monstrosEliminados = monstrosEliminados + 1
+		updateTexto()
+		
+		elseif (heroi.arma == self.id) then
+			if (self.id == 1 and self.x == 100) then
+				heroi:setSequence("verdeEsq_atirando")
+				heroi:play()
+				heroi:addEventListener("sprite", HeroiAtirar)
+				heroi.sentido = 1
+				elseif (self.id == 1 and self.x == 220) then
+				heroi:setSequence("verdeDir_atirando")
+				heroi:play()
+				heroi:addEventListener("sprite", HeroiAtirar)
+				heroi.sentido = 2
+				elseif (self.id == 2 and self.x == 100) then
+				heroi:setSequence("laranjaEsq_atirando")
+				heroi:play()
+				heroi:addEventListener("sprite", HeroiAtirar)
+				heroi.sentido = 1
+				elseif (self.id == 2 and self.x == 220) then
+				heroi:setSequence("laranjaDir_atirando")
+				heroi:play()
+				heroi:addEventListener("sprite", HeroiAtirar)
+				heroi.sentido = 2
+				-- elseif (self.id == 3 and self.x == 100) then
+				-- heroi:setSequence("rubikEsq_andando")
+				-- heroi:play()
+				-- heroi.sentido = 1
+				-- score=score+90
+				-- elseif (self.id == 3 and self.x == 220) then
+				-- heroi:setSequence("rubikDir_andando")
+				-- heroi:play()
+				-- heroi.sentido = 2
+				-- score=score+90
+			end
+		audio.play(somAtirando)
+		display.remove(self)
+		score=score+10
+		monstrosEliminados = monstrosEliminados + 1
+		updateTexto()
+		if (powerUp == true and heroi.powerUpAtivado == false) then
+			powerUp = false
+			criarRubik(self)
+		end
+		
+		end
+		
+		
+		return true;
+		end	
+		
+	
+	
+	textDistancia = display.newText("Distância: "..distancia.." m", display.contentWidth*0.01, 15, "VarelaRound-Regular", 15, left)
     textDistancia:setTextColor(0,0,255)
+	textScore = display.newText("Score: "..score, display.contentWidth*0.01, 2, "VarelaRound-Regular", 15, left)
+    textScore:setTextColor(0,0,255)
 	
 		local quadradovilao= {
 		   width = 82,
@@ -388,10 +743,19 @@
 	------ Sprite quadradovilao pulando --------------------
 
 	------- Sprite quadradovilao pulando fim--------------------
+			if (dificuldade >= 550) then
+			dificuldade = dificuldade - 5
+			end
+			local prob = contadorPowerUP + 10
+			local prob2 = (contadorPowerUP+1) * 10
 			
 			
 			local Fall = math.random(2) == 1 and 220 or 100
 			local monstro = math.random(1, 2)
+			
+			
+			
+			local probabilidadePowerUp = math.random(prob)
 			if (monstro == 1) then
 				vilao[i] = display.newSprite(sheet_quad, quad_pulando )
 				vilao[i].id = 1
@@ -399,6 +763,10 @@
 				vilao[i] = display.newSprite(sheet_quad2, quad_pulando )
 				vilao[i].id = 2
 			end
+				if (probabilidadePowerUp == prob and monstrosEliminados >= prob2 and heroi.powerUpAtivado == false 
+				and powerUp == false) then
+				rubikCube()
+				end
 				vilao[i].x = Fall
 				vilao[i].y = -30   	   	
 				vilao[i].value = i 	
@@ -412,7 +780,7 @@
 				Runtime:addEventListener("enterFrame", vilao[i])
 		
 			verificarMetro()
-
+			print (dificuldade)
 			i = i + 1
 			distancia = distancia + 1
 			updateTexto()
@@ -425,13 +793,13 @@
 			
 	end
 
-	
-			
+	 		
     end
-	
-	
-    vilaoTimer = timer.performWithDelay( 800, spawnQuad, -1 ) -- this is a time and 1000 is 1 second and 1000 miliseconds.. it will spawn one circle every one second.. you can change this at your own liking..
+	function vilao2Timer()
+	vilaoTimer = timer.performWithDelay( dificuldade,function () spawnQuad(); vilao2Timer() end, 1 ) -- this is a time and 1000 is 1 second and 1000 miliseconds.. it will spawn one circle every one second.. you can change this at your own liking..
 
+	end
+    vilao2Timer()
 ------- Spawning quadrados fim -------------------------
 
 		
